2aca572cc69756b32b557b869c203ba2266ef8a0
Sylvia <subin3311@gmail.com>
2020-09-17 16:24:59 +1200

    Set endpoints for events

--- digivent-backend/README.md
+++ digivent-backend/README.md
@@ -1,15 +1,13 @@
-#To start,
-1.Type npm init to terminul
+# To start,
 
-#Using the API
-2.create an .env file, add your Atlas connection URL:
+1. Type npm install to terminul
+
+# Using the API
+
+2. create an .env file, add your Atlas connection URL:
 
 ```
 MONGODB_URI=<YOUR_CONNECTION_URL>
 ```
 
-
-It looks like....
-```
-MONGODB_URI="mongodb+srv://sylvia:<password>@cluster0.syz6k.mongodb.net/sharing-app?retryWrites=true&w=majority"
-```
+3. npm run start
--- digivent-backend/routes/events.js
+++ digivent-backend/routes/events.js
@@ -2,15 +2,56 @@ const router = require("express").Router();
 
 const Event = require("../models/Event");
 
-
-router.get("/", (req, res, next) => {
-    Event.find({}) 
-    .sort({ createdAt: "desc" })
-    .then((results) => {
-      return res.send(results + "Events");
+router.param("id", (req, res, next, id) => {
+  Event.findById(id)
+    .then((event) => {
+      if (!event) {
+        res.status(404).send("Event not found");
+      } else {
+        req.event = event;
+        return next();
+      }
     })
     .catch(next);
 });
 
+router.get("/", (req, res, next) => {
+  Event.find({})
+    .sort({ createdAt: "desc" })
+    .then((results) => {
+      return res.send(results);
+    })
+    .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+  const event = new Event(req.body);
+  event
+    .save()
+    .then((result) => {
+      return res.status(201).send(result);
+    })
+    .catch(next);
+});
+
+router.get("/:id", (req, res, next) => {
+  return res.status(200).send(req.event);
+});
+
+router.put("/:id", (req, res, next) => {
+  Event.findByIdAndUpdate(req.event.id, req.body)
+    .then((event) => {
+      res.status(200).send(event);
+    })
+    .catch(next);
+});
+
+router.delete("/:id", (req, res, next) => {
+  Event.findByIdAndDelete(req.event.id)
+    .then((event) => {
+      res.status(204).send(event);
+    })
+    .catch(next);
+});
 
 module.exports = router;
